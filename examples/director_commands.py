b2b622441bb4435a62a506cb8aa0399781dc1743
stefanos <stliakis@gmail.com>
2020-11-07 14:44:19 +0200

    Command examples

--- examples/director_commands.py
+++ examples/director_commands.py
@@ -8,12 +8,12 @@ director = TelegramDirector("put your bot token here",
 
 @director.register(["restart nginx"], description="Restarts nginx server")
 def _(message):
-    message.reply_text("shell: " + str(os.system('sudo service nginx restart')))
+    message.reply_text("shell: " + str(os.system('service nginx restart')))
 
 
 @director.register(["update"], description="Updates the git repo")
 def _(message):
-    message.reply_text("shell: " + str(os.system('cd /home/stefanos/Desktop;touch test;')))
+    message.reply_text("shell: " + str(os.system('cd /home/someproject/;git fetch;git pull;service uwsgi restart;')))
 
 
 director.start()
